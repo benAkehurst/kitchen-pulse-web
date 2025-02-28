119b645cbdbc9c3cea0b7c50558aa78db9f180e2
benAkehurst <benakehurst1@gmail.com>
2025-02-28 16:36:05 +0000

    allow images from s3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kitchenpulse.s3.eu-west-2.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
