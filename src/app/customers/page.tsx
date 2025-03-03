247a8c1a7520828a7393ac17a3eed87f52fd6ba2
benAkehurst <benakehurst1@gmail.com>
2025-03-03 10:43:28 +0000

    customers and orders page working

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,4 +1,45 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useCustomer } from '@/hooks/useCustomer';
+import { Customer } from '@/types/Models';
+
+interface CustomerData {
+  customers: Customer[] | null;
+}
+
 export default function CustomersPage() {
+  const { getCustomers } = useCustomer();
+
+  const [data, setData] = useState<CustomerData>({
+    customers: [],
+  });
+  console.log('data: ', data);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [customers] = await Promise.all([getCustomers()]);
+        setData({ customers });
+      } catch (error) {
+        console.error('Error fetching customer data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data.customers && data.customers!.length === 0) {
+    return <div>No customers</div>;
+  }
+
   return (
     <div>
       <h1>Customers</h1>
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -3,28 +3,51 @@ import { useEffect, useState } from 'react';
 import { useOrders } from '@/hooks/useOrders';
 import { Order } from '@/types/Models';
 
+interface OrderData {
+  orders: Order[] | null;
+}
+
 export default function OrdersPage() {
   const { getAllOrders } = useOrders();
-  const [fetchedOrders, setFetchedOrders] = useState<Order[]>([]);
+  const [data, setData] = useState<OrderData>({
+    orders: [],
+  });
+  console.log('data: ', data);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchOrders = async () => {
-      const orders: Order[] = await getAllOrders();
-      console.log(orders);
-      setFetchedOrders(orders);
+    const fetchData = async () => {
+      try {
+        const [orders] = await Promise.all([getAllOrders()]);
+        setData({ orders });
+      } catch (error) {
+        console.error('Error fetching customer data:', error);
+      } finally {
+        setLoading(false);
+      }
     };
-    fetchOrders();
+
+    fetchData();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data.orders && data.orders.length === 0) {
+    return <div>No orders</div>;
+  }
 
   return (
     <div>
       <h1>Orders</h1>
-      {fetchedOrders.map((order) => (
-        <div key={order.orderId}>
-          <h2>{order.orderDate}</h2>
-          <p>{order.totalPrice}</p>
-        </div>
-      ))}
+      {data.orders &&
+        data.orders.map((order) => (
+          <div key={order.orderId}>
+            <h2>{order.orderDate}</h2>
+            <p>{order.totalPrice}</p>
+          </div>
+        ))}
     </div>
   );
 }
