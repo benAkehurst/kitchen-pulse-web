bbf2b13299424218828b5f9473f2a317d7005e07
benAkehurst <benakehurst1@gmail.com>
2025-03-03 10:43:04 +0000

    add logout button

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useUser } from '@/hooks/useUser';
+import { useAuth } from '@/hooks/useAuth';
 import { User } from '@/types/Models';
 import { useEffect, useState } from 'react';
 import UserProfileForm from '@/components/UserProfileForm';
 
 export default function AccountPage() {
   const { getUserInformation } = useUser();
+  const { logout } = useAuth();
   const [userInformation, setUserInformation] = useState<User>({
     email: '',
     name: '',
@@ -25,7 +27,15 @@ export default function AccountPage() {
 
   return (
     <div>
-      <h1 className="text-3xl mb-4">Account settings</h1>
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-3xl mb-4">Account settings</h1>
+        <button
+          className="bg-black text-white p-2 rounded hover:bg-gray-800"
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
+      </div>
       <div>
         <UserProfileForm initialData={userInformation} />
       </div>
