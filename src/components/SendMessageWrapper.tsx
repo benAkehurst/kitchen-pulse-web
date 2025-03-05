d4bc7c22f2ce49e3eb5fc299a66601b50c9cabac
benAkehurst <benakehurst1@gmail.com>
2025-03-05 16:33:19 +0000

    create components for modals

---
+++ src/components/SendMessageWrapper.tsx
@@ -0,0 +1,31 @@
+import { Customer, Order } from '@/types/Models';
+import Modal from './Modal';
+import SendMessageModal from './SendMessageModal';
+
+interface SendMessageWrapperProps {
+  customers: Customer[];
+  orders: Order[];
+}
+
+export default function SendMessageWrapper({
+  customers,
+  orders,
+}: SendMessageWrapperProps) {
+  return (
+    <>
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          // @ts-expect-error - HTML dialog method
+          document.getElementById('sendMessageModal')!.showModal()
+        }
+      >
+        Send a new message
+      </button>
+
+      <Modal customId="sendMessageModal">
+        <SendMessageModal customers={customers} orders={orders} />
+      </Modal>
+    </>
+  );
+}
---
+++ src/components/UpdateCustomerWrapper.tsx
@@ -0,0 +1,34 @@
+import { Customer } from '@/types/Models';
+import Modal from './Modal';
+import UpdateCustomerForm from './UpdateCustomerForm';
+
+interface UpdateCustomerWrapper {
+  handleUpdatedCustomer: (updatedCustomer: Customer) => void;
+  externalId: string;
+}
+
+export default function UpdateCustomerWrapper({
+  handleUpdatedCustomer,
+  externalId,
+}: UpdateCustomerWrapper) {
+  return (
+    <div className="mr-4">
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          // @ts-expect-error - HTML dialog method
+          document.getElementById('updateCustomerModal')!.showModal()
+        }
+      >
+        Update customer details
+      </button>
+
+      <Modal customId="updateCustomerModal">
+        <UpdateCustomerForm
+          onUpdateCustomer={handleUpdatedCustomer}
+          externalId={externalId!}
+        />
+      </Modal>
+    </div>
+  );
+}
