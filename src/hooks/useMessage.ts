46959dab34f2cc0e71f77fffeb7de112fa79f35f
benAkehurst <benakehurst1@gmail.com>
2025-03-05 17:25:21 +0000

    update useMessage hook

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,5 +1,5 @@
 import { api } from "@/lib/api";
-import { SendMessageData } from "@/types/Models";
+import { EditMessage, SendMessageData } from "@/types/Models";
 
 export const useMessages = () => {
   const getAllMessages = async () => {
@@ -7,17 +7,22 @@ export const useMessages = () => {
     return data;
   }
 
+  const getSingleMessage = async (externalId: string) => {
+    const { data } = await api.get(`/message/get-single-message?externalId=${externalId}`)
+    return data;
+  }
+
   const sendMessage = async (messageData: SendMessageData) => {
     return api.post("/message/send-new-message", messageData);
   };
 
-  const editMessage = async (id: string, newContent: string) => {
-    return api.put("/message/edit-message", { id, newContent });
+  const editMessage = async (externalId: string, newContent: EditMessage) => {
+    return api.put(`/message/edit-message?externalId=${externalId}`, newContent);
   };
 
   const deleteMessage = async (id: string) => {
     return api.delete(`/message/delete-message?externalId=${id}`);
   };
 
-  return { getAllMessages, sendMessage, editMessage, deleteMessage };
+  return { getAllMessages, getSingleMessage, sendMessage, editMessage, deleteMessage };
 };
