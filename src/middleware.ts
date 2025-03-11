43e55160b7e81ce946218a6b678c313cf49cec71
benAkehurst <benakehurst1@gmail.com>
2025-03-11 09:55:25 +0000

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("refreshToken")?.value;
+  const token = req.headers.get("Authorization");
 
   if (!token) {
     return NextResponse.redirect(new URL("/", req.url));
