c4a0ccf04a53344f3cd6ac3cca54ef859f8ed1d9
benAkehurst <benakehurst1@gmail.com>
2025-03-31 14:37:32 +0100

    update login page link

--- src/components/Utility/RegisterCTA.tsx
+++ src/components/Utility/RegisterCTA.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 
 export default function RegisterCTA() {
   return (
-    // TODO: ADD THE CORRECT LINK
-    <Link href="/" className="text-lg mt-2 mb-4">
+    <Link
+      href="https://www.kitchenpulse.co.uk/contact-us"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-lg mt-2 mb-4"
+    >
       Don&apos;t have an account?{' '}
       <span className="text-blue-800 font-bold">Contact us here</span>
     </Link>
